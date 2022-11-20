0f6a95e3ccdbe749b31295008368989a10b120bc
cty288 <tc3158@nyu.edu>
2022-11-20 03:11:47 -0500

    fix demo

--- web-app/app.py
+++ web-app/app.py
@@ -27,6 +27,6 @@ def upload():
     db.Image.insert_one(doc) # insert a new document
     docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
     '''
-    return render_template('photo_response_demo.html', imgBase64 = docs)
+    return render_template('photo_response_demo.html', imgBase64 = photo)
 
 app.run(debug = True)
