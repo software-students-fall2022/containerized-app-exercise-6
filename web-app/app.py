444ed726ad7298e256e47344e63bf779dd2d15b0
詹詹 <50549207+liushuchen2025@users.noreply.github.com>
2022-11-21 12:02:18 -0500

    setup web architecture

--- web-app/app.py
+++ web-app/app.py
@@ -1,34 +1,8 @@
-import pymongo
-from bson.objectid import ObjectId
-from flask import Flask, render_template, request, redirect, url_for, make_response, session, flash
-import datetime
-import os
+from flask import Flask
+from controllers.index import index_page
 
 app = Flask(__name__)
-app.secret_key = "secret key"
 
-client = pymongo.MongoClient("mongodb+srv://okkiris:F3iQz3hSCxOwhhOu@cluster0.ubegai3.mongodb.net/?retryWrites=true&w=majority")
-db=client["Team6"]
-
-app.config.from_pyfile( "config/base_setting.py" )
-
-@app.route('/')
-def home():
-    return render_template('/photo/photo_demo.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    photo = request.form.get('photo')
-    #machine learning stuff here
-    #save image to Mongodb
-    '''
-    doc = {
-            "Image": photo,
-            "created_at": datetime.datetime.utcnow()
-    }
-    db.Image.insert_one(doc) # insert a new document
-    docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
-    '''
-    return render_template('/photo/photo_response_demo.html', imgBase64 = photo)
+app.register_blueprint( index_page )
 
 app.run(debug = True)
--- web-app/controllers/index.py
+++ web-app/controllers/index.py
@@ -1,8 +1,10 @@
 import pymongo
 from bson.objectid import ObjectId
-from flask import Flask, render_template, request, Blueprint
+from flask import Flask, render_template, request, Blueprint, redirect, url_for, make_response, session, flash
 import datetime
+import os
 
+index_page = Blueprint( "index_page", __name__ )
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -10,19 +12,11 @@ app.secret_key = "secret key"
 client = pymongo.MongoClient("mongodb+srv://okkiris:F3iQz3hSCxOwhhOu@cluster0.ubegai3.mongodb.net/?retryWrites=true&w=majority")
 db=client["Team6"]
 
-index_page = Blueprint( "index_page", __name__)
-
-@index_page.route("/")
-def index_page_index():
-    return "indexpage"
-
-app.register_blueprint( index_page, url_prefix = "/" )
-
-@app.route('/')
+@index_page.route('/')
 def home():
-    return render_template('/common/layout.html')
+    return render_template('/photo/photo_demo.html')
 
-@app.route('/upload', methods=['POST'])
+@index_page.route('/upload', methods=['POST'])
 def upload():
     photo = request.form.get('photo')
     #machine learning stuff here
@@ -35,6 +29,4 @@ def upload():
     db.Image.insert_one(doc) # insert a new document
     docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
     '''
-    return render_template('photo/photo_response_demo.html', imgBase64 = docs)
-
-app.run(debug = True)
+    return render_template('/photo/photo_response_demo.html', imgBase64 = photo)
