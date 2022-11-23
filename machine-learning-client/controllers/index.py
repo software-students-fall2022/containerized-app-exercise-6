d8b7b53241390f9b22b0c7dea65c9987cda54b42
cty288 <tc3158@nyu.edu>
2022-11-23 03:29:03 -0500

    new demo

--- machine-learning-client/controllers/index.py
+++ machine-learning-client/controllers/index.py
@@ -6,10 +6,11 @@ import os
 import face_recognition
 import os
 import matplotlib.pyplot as plt 
-from PIL import Image
+from PIL import Image, ImageDraw
 from io import BytesIO
 import base64
 import re
+import numpy
 
 index_page = Blueprint( "index_page", __name__ )
 
@@ -27,18 +28,8 @@ def home():
 def gallery():
     return ""
 
-@index_page.route('/upload', methods=['POST'])
-def upload():
-    photo = request.form.get('photo')
-    
-    
-    #print(photo)
-    #machine learning stuff here
-    #save image to Mongodb
-    
-    
-    #transform photo to bytes for machine learning
-    image_data = re.sub('^data:image/.+;base64,', '', photo)
+
+def face_detect(image_data):
     image = face_recognition.load_image_file(BytesIO(base64.b64decode(image_data)))
     face_locations = face_recognition.face_locations(image) 
     
@@ -50,12 +41,97 @@ def upload():
 
         # get face
         face_image = image[top:bottom, left:right]
-        pil_image = Image.fromarray(face_image)
-        # jupyter 绘图
+        image = Image.fromarray(face_image)
+      
         # pil_image.show()
-        plt.imshow(pil_image)
-        plt.axis('off')    
-        plt.show()
+        #plt.imshow(image)
+        #plt.axis('off')    
+        #plt.show()
+   
+    if(len(face_locations) > 0):
+        buffer = BytesIO()
+        image.save(buffer,format="PNG")                  
+        myimage = buffer.getvalue()   
+        myimage = "data:image/png;base64," + base64.b64encode(myimage).decode('utf-8')
+        
+        return myimage
+    return None
+
+def face_features(image_data):
+    image = face_recognition.load_image_file(BytesIO(base64.b64decode(image_data)))
+    face_landmarks_list = face_recognition.face_landmarks(image)
+    
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
+    
+    for face_landmarks in face_landmarks_list:
+        for facial_feature in face_landmarks.keys():
+            d.line(face_landmarks[facial_feature], width=5)
+
+    if(len(face_landmarks_list) > 0):
+        buffer = BytesIO()
+        pil_image.save(buffer,format="PNG")                  
+        myimage = buffer.getvalue()   
+        myimage = "data:image/png;base64," + base64.b64encode(myimage).decode('utf-8')
+        
+        return myimage
+    return None
+    
+
+def face_color(image_data):
+    image = face_recognition.load_image_file(BytesIO(base64.b64decode(image_data)))
+     
+    face_landmarks_list = face_recognition.face_landmarks(image)
+
+    pil_image = Image.fromarray(image)
+
+    # 绘图
+    for face_landmarks in face_landmarks_list:
+        d = ImageDraw.Draw(pil_image, 'RGBA')
+
+        # eyebrow
+        d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
+        d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+        d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
+        d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+
+        # lip
+        d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+        d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+        d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+        d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+
+        # eye
+        d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255, 30))
+        d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255, 30))
+
+        # eye outline
+        d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+        d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    
+    
+    if(len(face_landmarks_list) > 0):
+        buffer = BytesIO()
+        pil_image.save(buffer,format="PNG")                  
+        myimage = buffer.getvalue()   
+        myimage = "data:image/png;base64," + base64.b64encode(myimage).decode('utf-8')
+        
+        return myimage
+    return None
+
+
+@index_page.route('/upload', methods=['POST'])
+def upload():
+    photo = request.form.get('photo')
+    
+    
+    #print(photo)
+    #machine learning stuff here
+    #save image to Mongodb
+ 
+    
+    #transform photo to bytes for machine learning
+    image_data = re.sub('^data:image/.+;base64,', '', photo)
     
     """
     doc = {
@@ -65,5 +141,10 @@ def upload():
     db.Image.insert_one(doc) # insert a new document
     docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
     """
-    return render_template('/photo/photo_response_demo.html', imgBase64 = photo)
+    
+    #img = face_detect(image_data)   
+    img = face_features(image_data)
+    #img = face_color(image_data)
+    return render_template('/photo/photo_response_demo.html', imgBase64 = img)
+    
 
