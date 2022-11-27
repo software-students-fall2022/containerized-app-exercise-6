bf7a858171a0759cc136a7b4d995af53a2d5cce6
fwenni <wf630@nyu.edu>
2022-11-27 00:34:30 -0500

    fix otuput

--- web-app/controllers/index.py
+++ web-app/controllers/index.py
@@ -37,81 +37,46 @@ def compute_percentage():
         'Surprise':7
     }
 
-    docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
-
-    total=0
-
-    count_array=[0,0,0,0,0,0,0,0]
+    docs = db.TestImage.find({}).sort("created_at", -1).limit(1) # sort in descending order of created_at timestamp
 
     for doc in docs:
+        
+        docs_id=doc['_id']
+
+        total=0
+
+        count_array=[0,0,0,0,0,0,0,0]
 
         for result in doc['results']:
-            
+
             total+=1
-            
+                
             emotion=emotion_dict[result['emotion']]
 
             count_array[emotion]+=1
 
-    output=[0,0,0,0,0,0,0,0]
+        output=[0,0,0,0,0,0,0,0]
 
-    for c in range(len(count_array)):
+        for c in range(len(count_array)):
 
-        output[c]=count_array[c]/total
+            output[c]=count_array[c]/total
 
-    return output
-'''
-def compute_median():
+        key_list = list(emotion_dict.keys())
 
-    docs = db.result.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+        val_list = list(emotion_dict.values())
 
-    total=0
+        output_dict={}
 
-    count_array=[0,0,0,0,0,0,0,0]
+        for n in range(len(output)):
 
-    for doc in docs:
+            position = val_list.index(n)
 
-        total+=1
+            output_dict[key_list[position]]=output[n]
 
-        count_array[doc['emotion']]+=1
-
-    index=total / 2
-
-    count=0
-
-    for c in count_array:
-
-        if (index-c<=0):
-
-            output=count
-
-        else:
-
-            index=index-c
-
-            count+=1
+        db.TestImage.update({"_id" :ObjectId(docs_id)},{"$set":{"Percentage":output_dict}})
 
     return output
 
-def compute_mean():
-
-    docs = db.result.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
-
-    total=0
-
-    count = 0
-
-    for doc in docs:
-
-        total+=doc['emotion']
-
-        count += 1
-
-    output=total/count
-
-    return output
-
-'''
 def find_max():
 
     emotion_dict={
@@ -123,15 +88,16 @@ def find_max():
         'Neutral':5,
         'Sadness':6,
         'Surprise':7
-    } # sort in descending order of created_at timestamp
+    }
 
-    docs = db.Image.find({}).sort("created_at", -1)
-
+    docs = db.TestImage.find({}).sort("created_at", -1).limit(1)
 
     count_array=[0,0,0,0,0,0,0,0]
     output=[]
 
     for doc in docs:
+
+        docs_id=doc['_id']
 
         for result in doc['results']:
             
@@ -139,23 +105,26 @@ def find_max():
 
             count_array[emotion]+=1
 
-    max = 0
-    for c in range(len(count_array)):
-        if (count_array[c] > max):
-            max = count_array[c]
+        max = 0
+        for c in range(len(count_array)):
+            if (count_array[c] > max):
+                max = count_array[c]
 
 
-    for i in range(len(count_array)):
-        if (count_array[i] == max):
-            output.append(i)
-    
-    key_list = list(emotion_dict.keys())
-    val_list = list(emotion_dict.values())
+        for i in range(len(count_array)):
+            if (count_array[i] == max):
+                output.append(i)
+        
+        key_list = list(emotion_dict.keys())
+        val_list = list(emotion_dict.values())
 
 
-    for j in range(len(output)):
-        position = val_list.index(output[j])
-        output[j] = key_list[position]
+        for j in range(len(output)):
+            position = val_list.index(output[j])
+            output[j] = key_list[position]
+
+        db.TestImage.update({"_id" :ObjectId(docs_id)},{"$set":{"MaxEmotion":output}})
+
     return output
 
 def find_min():
@@ -169,16 +138,16 @@ def find_min():
         'Neutral':5,
         'Sadness':6,
         'Surprise':7
-    } # sort in descending order of created_at timestamp
+    }
 
-    docs = db.Image.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
-
-    count = 0
+    docs = db.TestImage.find({}).sort("created_at", -1).limit(1) # sort in descending order of created_at timestamp
 
     count_array=[0,0,0,0,0,0,0,0]
     output=[]
 
     for doc in docs:
+
+        docs_id=doc['_id']
 
         for result in doc['results']:
             
@@ -186,35 +155,37 @@ def find_min():
 
             count_array[emotion]+=1
 
-    min = sys.maxsize
-    for c in range(len(count_array)):
-        if (count_array[c] < min):
-            min = count_array[c]
+        min = sys.maxsize
+        for c in range(len(count_array)):
+            if (count_array[c] < min):
+                min = count_array[c]
 
-    for i in range(len(count_array)):
-        if (count_array[i] == min):
-            output.append(i)
+        for i in range(len(count_array)):
+            if (count_array[i] == min):
+                output.append(i)
 
-    key_list = list(emotion_dict.keys())
-    val_list = list(emotion_dict.values())
+        key_list = list(emotion_dict.keys())
+        val_list = list(emotion_dict.values())
 
 
-    for j in range(len(output)):
-        position = val_list.index(output[j])
-        output[j] = key_list[position]
-    return output
+        for j in range(len(output)):
+            position = val_list.index(output[j])
+            output[j] = key_list[position]
+
+        db.TestImage.update({"_id" :ObjectId(docs_id)},{"$set":{"MinEmotion":output}})
 
     return output
 
 @index_page.route('/')
 def home():
-
+    
     percentage=compute_percentage()
     MaxNumber=find_max()
     MinNumber=find_min()
 
-    return render_template('/photo/test_result.html', docs = MinNumber)
-    
+    docs = db.TestImage.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+
+    return render_template('/photo/test_result.html', docs = docs)
 '''
     doc = {
             "original": 'photo',
@@ -223,29 +194,16 @@ def home():
             {"img":'img',"emotion":'Happiness'},{"img":'img',"emotion":'Surprise'}]
         }
 
-    db.Image.insert_one(doc)
+    db.TestImage.insert_one(doc)
+
+    doc = {
+            "original": 'photo',
+            "results": [{"img":'img',"emotion":'Anger'},{"img":'img',"emotion":'Neutral'},
+            {"img":'img',"emotion":'Fear'}, {"img":'img',"emotion":'Neutral'},{"img":'img',"emotion":'Sadness'}]
+        }
+
+    db.TestImage.insert_one(doc)
 '''
-
-'''
-    percentage=compute_percentage()
-
-    mean=compute_mean()
-
-    median=compute_median()
-
-    MaxNumber=find_max()
-
-    MinNumber=find_min()
-
-    dictionary={
-        "Percentage":percentage,
-        "Mean":mean,
-        "Median":median,
-        "MaxNumber of emotion":MaxNumber,
-        "MinNumber of emotion": MinNumber
-    }
-'''
-    #return render_template('/photo/test_result.html', docs = "hahaha")
 
     #return render_template('/photo/photo_demo.html')
 
